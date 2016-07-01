6a784085e18d2e8a218841c57aea0182dc359168
Roman Alshehri <aalshehri08@hotmail.com>
2016-07-01 14:04:26 +0300

    jade

--- config/express.js
+++ config/express.js
@@ -1,4 +1,6 @@
 var config = require('./config'),
+    http = require('http'),
+    socketio = require('socket.io'),
     express = require('express'),
     morgan = require('morgan'),
     compress = require('compression'),
@@ -6,11 +8,14 @@ var config = require('./config'),
     methodOverride = require('method-override'),
     session = require('express-session'),
     flash = require('connect-flash');
-    passport = require('passport');
+    passport = require('passport'),
+    MongoStore = require('connect-mongo')(session);
 
 
-module.exports = function () {
+module.exports = function (db) {
   var app = express();
+  var server = http.createServer(app);
+  var io = socketio.listen(server);
 
   if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
@@ -21,20 +26,29 @@ module.exports = function () {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
+
   app.use(bodyParser.json());
   app.use(methodOverride());
 
-  app.use(session({
-    saveUninitialized: true,
-    resave: true,
-    secret: config.sessionSecret
-  }));
+
+  var mongoStore = new MongoStore({
+        // db: db.connection.db
+        mongooseConnection: db.connection
+    });
+
+	// Configure the 'session' middleware
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret,
+		store: mongoStore
+	}));
 
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
 
   app.use(flash());
-  
+
   app.use(passport.initialize());
   app.use(passport.session());
 
@@ -44,6 +58,7 @@ module.exports = function () {
   require('../app/routes/users.server.routes.js')(app);
   require('../app/routes/posts.server.routes.js')(app);
   require('../app/routes/articles.server.routes.js')(app);
+  // require('./socketio')(server, io, mongoStore);
 
   // call for static files always below the call of routes to make faster
   app.use(express.static('./public'));
--- public/application.js
+++ public/application.js
@@ -6,7 +6,7 @@ var mainApplicationModuleName = 'mean';
 
 // Create the main application
 var mainApplicationModule = angular.module(mainApplicationModuleName,
-	['ngRoute', 'ngResource', 'example', 'users', 'articles']);
+	['ngRoute', 'ngResource', 'example', 'users', 'articles', 'chat']);
 
 // Configure the hashbang URLs using the $locationProvider services
 mainApplicationModule.config(['$locationProvider',
